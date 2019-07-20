765abd2f1125d40a646a4decb24389c4b01bed6b
mhdjanuar <mhdjanuar.17@gmail.com>
2019-07-20 21:16:19 +0700

    change migration user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -9,6 +9,7 @@ class UserSchema extends Schema {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
+      table.string('no_hp',15).notNullable().unique()
       table.string('password', 60).notNullable()
       table.timestamps()
     })
